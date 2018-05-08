c4eda4fc18ab87e9c8582cfa0e86e3135a8403bf
alex <alex@brickbridgeconsulting.com>
2018-05-08 16:14:39 -0400

    * AddItemFunction.cs: * AddItemHandler.csproj: using PodioCore
    * aws-lambda-tools-defaults.json: defaults

--- PodioDatabaseHandlers/src/PodioDatabaseHandlers/MySqlQueryHandler.cs
+++ PodioDatabaseHandlers/src/PodioDatabaseHandlers/MySqlQueryHandler.cs
@@ -7,7 +7,7 @@ using MySql.Data.MySqlClient;
 using Amazon.Lambda.Core;
 namespace BrickBridge.Lambda.MySql
 {
-    public class MySqlQueryHandler
+    public class MySqlQueryHandler : IDisposable
     {
         private ILambdaContext _context;
         private string _connStr = System.Environment.GetEnvironmentVariable("PODIO_DB_CONNECTION_STRING");
@@ -17,6 +17,7 @@ namespace BrickBridge.Lambda.MySql
         {
             _context = context;
             _conn = new MySqlConnection(connectionString ?? _connStr);
+            _conn.Open();
         }
 
         private async Task ExecuteNonQuery(MySqlCommand command)
@@ -24,18 +25,15 @@ namespace BrickBridge.Lambda.MySql
             try
             {
                 _context.Logger.LogLine("Trying to connect to MySQL...");
-                _conn.Open();
                 _context.Logger.LogLine($"Executing command: {command.CommandText}");
                 var affected = await command.ExecuteNonQueryAsync();
                 _context.Logger.LogLine($"{affected} rows affected");
             }
             catch (Exception e)
             {
-                _context.Logger.LogLine(e.ToString());
-            }
-            finally
-            {
-                await _conn.CloseAsync();
+                _conn.Close();
+                _context.Logger.LogLine($"Exception in ExecuteNonQuery: {e.ToString()}");
+                throw e;
             }
         }
 
@@ -45,57 +43,63 @@ namespace BrickBridge.Lambda.MySql
             try
             {
                 _context.Logger.LogLine("Trying to connect to MySQL...");
-                _conn.Open();
                 _context.Logger.LogLine($"Executing command: {command.CommandText}");
-                var reader = await command.ExecuteReaderAsync();
+                result = await command.ExecuteReaderAsync();
+                //if (result.HasRows)
+                    //_context.Logger.LogLine($"reader returned rows");
             }
             catch (Exception e)
             {
-                _context.Logger.LogLine(e.ToString());
+                _conn.Close();
+                _context.Logger.LogLine($"Exception in ExecuteQuery: {e.ToString()}");
+                throw e;
             }
-            finally
-            {
-                await _conn.CloseAsync();
-            }
-
             return result;
         }
 
-        private async Task<T> ExecuteScalar<T>(MySqlCommand command)
+        private async Task<object> ExecuteScalar(MySqlCommand command)
         {
             object result = null;
             try
             {
                 _context.Logger.LogLine("Trying to connect to MySQL...");
-                _conn.Open();
                 _context.Logger.LogLine($"Executing command: {command.CommandText}");
                 result = await command.ExecuteScalarAsync();
             }
             catch (Exception e)
             {
-                _context.Logger.LogLine(e.ToString());
+                _conn.Close();
+                _context.Logger.LogLine($"Exception in ExecuteScalar: {e.ToString()}");
+                throw e;
             }
-            finally
-            {
-                await _conn.CloseAsync();
-            }
-
-            return (T)result;
+            return result;
         }
 
-        public async Task<int> GetPodioAppId(string bbcApp, string version, string spaceName, string appName)
+        private async Task<UInt64> ExecuteUInt64(MySqlCommand cmd)
         {
-            var cmd = new MySqlCommand(MySqlQueries.SELECT_APP_ID + MySqlQueries.GET_ID, _conn);
+            var result = await ExecuteScalar(cmd);
+            return (UInt64)result;
+        }
+
+        private async Task<Int32> ExecuteInt32(MySqlCommand cmd)
+        {
+            var result = await ExecuteScalar(cmd);
+            return (Int32)result;
+        }
+
+        public async Task<Int32> GetPodioAppId(string bbcApp, string version, string spaceName, string appName)
+        {
+            var cmd = new MySqlCommand(MySqlQueries.SELECT_APP_ID, _conn);
             cmd.Parameters.Add("?bbcAppId", MySqlDbType.VarChar).Value = bbcApp;
             cmd.Parameters.Add("?version", MySqlDbType.VarChar).Value = version;
             cmd.Parameters.Add("?appName", MySqlDbType.VarChar).Value = appName;
             cmd.Parameters.Add("?spaceName", MySqlDbType.VarChar).Value = spaceName;
-            var podioAppId = await ExecuteScalar<Int32>(cmd);
+            var podioAppId = await ExecuteInt32(cmd);
             _context.Logger.LogLine($"PodioAppId = {podioAppId}");
             return podioAppId;
         }
 
-        public async Task<int> InsertPodioItem(int podioAppId, int itemId, int revision, string clientId, string envId)
+        public async Task<UInt64> InsertPodioItem(Int32 podioAppId, int itemId, int revision, string clientId, string envId)
         {
             var cmd = new MySqlCommand(MySqlQueries.INSERT_ITEM + MySqlQueries.GET_ID, _conn);
             cmd.Parameters.Add("?podioAppId", MySqlDbType.Int32).Value = podioAppId;
@@ -103,18 +107,20 @@ namespace BrickBridge.Lambda.MySql
             cmd.Parameters.Add("?revision", MySqlDbType.Int32).Value = revision;
             cmd.Parameters.Add("?clientId", MySqlDbType.VarChar).Value = clientId;
             cmd.Parameters.Add("?envId", MySqlDbType.VarChar).Value = envId;
-            var newItemId = await ExecuteScalar<Int32>(cmd);
+            var newItemId = await ExecuteUInt64(cmd);
             _context.Logger.LogLine($"PodioItemId = {newItemId}");
             return newItemId;
         }
 
-        public async Task<List<MySqlPodioField>> SelectAppFields(int podioAppId)
+        public async Task<List<MySqlPodioField>> SelectAppFields(Int32 podioAppId)
         {
             var cmd = new MySqlCommand(MySqlQueries.SELECT_APP_FIELDS, _conn);
+
             cmd.Parameters.Add("?podioAppId", MySqlDbType.Int32).Value = podioAppId;
             var reader = await ExecuteQuery(cmd);
             var result = new List<MySqlPodioField>();
-            while(reader.Read())
+            _context.Logger.LogLine($"Reader has rows: {reader.HasRows}");
+            while (reader.Read())
             {
                 result.Add(new MySqlPodioField
                 {
@@ -123,94 +129,95 @@ namespace BrickBridge.Lambda.MySql
                     Type = reader.GetString(2)
                 });
             }
+            reader.Close();
             _context.Logger.LogLine($"App has {result.Count} fields.");
             return result;
         }
 
-        public async Task<int> InsertCategoryField(int itemId, int fieldId, string optionText, int optionId)
+        public async Task<UInt64> InsertCategoryField(ulong itemId, int fieldId, string optionText, int optionId)
         {
             var cmd = new MySqlCommand(MySqlQueries.INSERT_CATEGORY_DATA + MySqlQueries.GET_ID, _conn);
             cmd.Parameters.Add("?itemId", MySqlDbType.Int32).Value = itemId;
             cmd.Parameters.Add("?fieldId", MySqlDbType.Int32).Value = fieldId;
             cmd.Parameters.Add("?optionText", MySqlDbType.VarChar).Value = optionText;
             cmd.Parameters.Add("?optionId", MySqlDbType.Int32).Value = optionId;
-            var fieldDataId = await ExecuteScalar<Int32>(cmd);
+            var fieldDataId = await ExecuteUInt64(cmd);
             return fieldDataId;
         }
 
-        public async Task<int> InsertContactField(int itemId, int fieldId, int contactId)
+        public async Task<UInt64> InsertContactField(ulong itemId, int fieldId, int contactId)
         {
             var cmd = new MySqlCommand(MySqlQueries.INSERT_CONTACT_DATA + MySqlQueries.GET_ID, _conn);
             cmd.Parameters.Add("?itemId", MySqlDbType.Int32).Value = itemId;
             cmd.Parameters.Add("?fieldId", MySqlDbType.Int32).Value = fieldId;
             cmd.Parameters.Add("?contactId", MySqlDbType.Int32).Value = contactId;
-            var fieldDataId = await ExecuteScalar<Int32>(cmd);
+            var fieldDataId = await ExecuteUInt64(cmd);
             return fieldDataId;
         }
 
-        public async Task<int> InsertDateField(int itemId, int fieldId, DateTime? start, DateTime? end)
+        public async Task<UInt64> InsertDateField(ulong itemId, int fieldId, DateTime? start, DateTime? end)
         {
             var cmd = new MySqlCommand(MySqlQueries.INSERT_DATE_DATA + MySqlQueries.GET_ID, _conn);
             cmd.Parameters.Add("?itemId", MySqlDbType.Int32).Value = itemId;
             cmd.Parameters.Add("?fieldId", MySqlDbType.Int32).Value = fieldId;
             cmd.Parameters.Add("?start", MySqlDbType.DateTime).Value = start;
             cmd.Parameters.Add("?end", MySqlDbType.DateTime).Value = end;
-            var fieldDataId = await ExecuteScalar<Int32>(cmd);
+            var fieldDataId = await ExecuteUInt64(cmd);
             return fieldDataId;
         }
 
-        public async Task<int> InsertDurationField(int itemId, int fieldId, int duration)
+        public async Task<UInt64> InsertDurationField(ulong itemId, int fieldId, int duration)
         {
             var cmd = new MySqlCommand(MySqlQueries.INSERT_DURATION_DATA + MySqlQueries.GET_ID, _conn);
             cmd.Parameters.Add("?itemId", MySqlDbType.Int32).Value = itemId;
             cmd.Parameters.Add("?fieldId", MySqlDbType.Int32).Value = fieldId;
             cmd.Parameters.Add("?duration", MySqlDbType.Int32).Value = duration;
-            var fieldDataId = await ExecuteScalar<Int32>(cmd);
+            var fieldDataId = await ExecuteUInt64(cmd);
             return fieldDataId;
         }
 
-        public async Task<int> InsertLocationField(int itemId, int fieldId, string location)
+        public async Task<UInt64> InsertLocationField(ulong itemId, int fieldId, string location)
         {
             var cmd = new MySqlCommand(MySqlQueries.INSERT_LOCATION_DATA + MySqlQueries.GET_ID, _conn);
             cmd.Parameters.Add("?itemId", MySqlDbType.Int32).Value = itemId;
             cmd.Parameters.Add("?fieldId", MySqlDbType.Int32).Value = fieldId;
             cmd.Parameters.Add("?location", MySqlDbType.VarChar).Value = location;
-            var fieldDataId = await ExecuteScalar<Int32>(cmd);
+            var fieldDataId = await ExecuteUInt64(cmd);
             return fieldDataId;
         }
 
-        public async Task<int> InsertMemberField(int itemId, int fieldId, int memberId)
+        public async Task<UInt64> InsertMemberField(ulong itemId, int fieldId, int memberId)
         {
             var cmd = new MySqlCommand(MySqlQueries.INSERT_MEMBER_DATA + MySqlQueries.GET_ID, _conn);
             cmd.Parameters.Add("?itemId", MySqlDbType.Int32).Value = itemId;
             cmd.Parameters.Add("?fieldId", MySqlDbType.Int32).Value = fieldId;
             cmd.Parameters.Add("?memberId", MySqlDbType.Int32).Value = memberId;
-            var fieldDataId = await ExecuteScalar<Int32>(cmd);
+            var fieldDataId = await ExecuteUInt64(cmd);
             return fieldDataId;
         }
 
-        public async Task<int> InsertMoneyField(int itemId, int fieldId, decimal amount, string currency)
+        public async Task<UInt64> InsertMoneyField(ulong itemId, int fieldId, decimal amount, string currency)
         {
             var cmd = new MySqlCommand(MySqlQueries.INSERT_MONEY_DATA + MySqlQueries.GET_ID, _conn);
             cmd.Parameters.Add("?itemId", MySqlDbType.Int32).Value = itemId;
             cmd.Parameters.Add("?fieldId", MySqlDbType.Int32).Value = fieldId;
             cmd.Parameters.Add("?amount", MySqlDbType.Decimal).Value = amount;
             cmd.Parameters.Add("?currency", MySqlDbType.VarChar).Value = currency;
-            var fieldDataId = await ExecuteScalar<Int32>(cmd);
+            var fieldDataId = await ExecuteUInt64(cmd);
             return fieldDataId;
         }
 
-        public async Task<int> InsertNumberField(int itemId, int fieldId, double value)
+        public async Task<UInt64> InsertNumberField(ulong itemId, int fieldId, double value)
         {
             var cmd = new MySqlCommand(MySqlQueries.INSERT_NUMBER_DATA + MySqlQueries.GET_ID, _conn);
             cmd.Parameters.Add("?itemId", MySqlDbType.Int32).Value = itemId;
             cmd.Parameters.Add("?fieldId", MySqlDbType.Int32).Value = fieldId;
             cmd.Parameters.Add("?value", MySqlDbType.Double).Value = value;
-            var fieldDataId = await ExecuteScalar<Int32>(cmd);
+            var fieldDataId = await ExecuteUInt64(cmd);
             return fieldDataId;
         }
 
-        public async Task<int> InsertPhoneField(int itemId, int fieldId, string type, string value)
+        public async Task<UInt64> InsertPhoneField(ulong itemId, int fieldId, string type, string value)
         {
             var cmd = new MySqlCommand(MySqlQueries.INSERT_PHONEEMAIL_DATA + MySqlQueries.GET_ID, _conn);
             cmd.Parameters.Add("?itemId", MySqlDbType.Int32).Value = itemId;
@@ -218,11 +225,11 @@ namespace BrickBridge.Lambda.MySql
             cmd.Parameters.Add("?type", MySqlDbType.VarChar).Value = type;
             cmd.Parameters.Add("?value", MySqlDbType.VarChar).Value = value;
             cmd.Parameters.Add("?phoneOrEmail", MySqlDbType.VarChar).Value = "P";
-            var fieldDataId = await ExecuteScalar<Int32>(cmd);
+            var fieldDataId = await ExecuteUInt64(cmd);
             return fieldDataId;
         }
 
-        public async Task<int> InsertEmailField(int itemId, int fieldId, string type, string value)
+        public async Task<UInt64> InsertEmailField(ulong itemId, int fieldId, string type, string value)
         {
             var cmd = new MySqlCommand(MySqlQueries.INSERT_PHONEEMAIL_DATA + MySqlQueries.GET_ID, _conn);
             cmd.Parameters.Add("?itemId", MySqlDbType.Int32).Value = itemId;
@@ -230,38 +237,74 @@ namespace BrickBridge.Lambda.MySql
             cmd.Parameters.Add("?type", MySqlDbType.VarChar).Value = type;
             cmd.Parameters.Add("?value", MySqlDbType.VarChar).Value = value;
             cmd.Parameters.Add("?phoneOrEmail", MySqlDbType.VarChar).Value = "E";
-            var fieldDataId = await ExecuteScalar<Int32>(cmd);
+            var fieldDataId = await ExecuteUInt64(cmd);
             return fieldDataId;
         }
 
-        public async Task<int> InsertProgressField(int itemId, int fieldId, int progress)
+        public async Task<UInt64> InsertProgressField(ulong itemId, int fieldId, int progress)
         {
             var cmd = new MySqlCommand(MySqlQueries.INSERT_PROGRESS_DATA + MySqlQueries.GET_ID, _conn);
             cmd.Parameters.Add("?itemId", MySqlDbType.Int32).Value = itemId;
             cmd.Parameters.Add("?fieldId", MySqlDbType.Int32).Value = fieldId;
             cmd.Parameters.Add("?progress", MySqlDbType.Int32).Value = progress;
-            var fieldDataId = await ExecuteScalar<Int32>(cmd);
+            var fieldDataId = await ExecuteUInt64(cmd);
             return fieldDataId;
         }
 
-        public async Task<int> InsertRelationField(int itemId, int fieldId, int refId)
+        public async Task<UInt64> InsertRelationField(ulong itemId, int fieldId, int refId)
         {
             var cmd = new MySqlCommand(MySqlQueries.INSERT_RELATION_DATA + MySqlQueries.GET_ID, _conn);
             cmd.Parameters.Add("?itemId", MySqlDbType.Int32).Value = itemId;
             cmd.Parameters.Add("?fieldId", MySqlDbType.Int32).Value = fieldId;
             cmd.Parameters.Add("?ref", MySqlDbType.Int32).Value = refId;
-            var fieldDataId = await ExecuteScalar<Int32>(cmd);
+            var fieldDataId = await ExecuteUInt64(cmd);
             return fieldDataId;
         }
 
-        public async Task<int> InsertTextField(int itemId, int fieldId, string text)
+        public async Task<UInt64> InsertTextField(ulong itemId, int fieldId, string text)
         {
             var cmd = new MySqlCommand(MySqlQueries.INSERT_TEXT_DATA + MySqlQueries.GET_ID, _conn);
             cmd.Parameters.Add("?itemId", MySqlDbType.Int32).Value = itemId;
             cmd.Parameters.Add("?fieldId", MySqlDbType.Int32).Value = fieldId;
-            cmd.Parameters.Add("?text", MySqlDbType.Int32).Value = text;
-            var fieldDataId = await ExecuteScalar<Int32>(cmd);
+            cmd.Parameters.Add("?text", MySqlDbType.VarChar).Value = text;
+            var fieldDataId = await ExecuteUInt64(cmd);
             return fieldDataId;
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _conn.Close();
+                    _conn = null;
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~MySqlQueryHandler() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
