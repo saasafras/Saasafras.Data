201d1e61bb8c5f21fa96075bdd42ee57bb5ec354
alex <alex@brickbridgeconsulting.com>
2018-10-23 15:57:48 -0400

    * aws-lambda-tools-defaults.json: new function
    * SaasafrasAppTableCreate.csproj: nuget
    
    * SaasafrasAppTableCreateFunction.cs: add table when app change
      detected

--- SaasafrasAppTableCreate/src/SaasafrasAppTableCreate/SaasafrasAppTableCreateFunction.cs
+++ SaasafrasAppTableCreate/src/SaasafrasAppTableCreate/SaasafrasAppTableCreateFunction.cs
@@ -2,26 +2,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using BrickBridge.Models;
 using Amazon.Lambda.Core;
+using BrickBridge.Lambda.MySql;
+using PodioCore.Applications;
+using PodioCore.Spaces;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
-namespace SaasafrasAppTableCreate
+namespace BrickBridge.Lambda
 {
-    public class Function
+    public class SaasafrasAppTableCreateRequest
     {
-        
+        public string appId { get; set; }
+        public string version { get; set; }
+    }
+
+    public class SaasafrasAppTableCreateFunction
+    {
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public async Task FunctionHandler(RoutedPodioEvent input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            context.Logger.LogLine($"Entered function...");
+            ILambdaSerializer serializer = new Amazon.Lambda.Serialization.Json.JsonSerializer();
+
+            using (var _mysql = new MySqlQueryHandler(context))
+            {
+                context.Logger.LogLine($"Creating view and table, populating view for {input.appId}, {input.version}, {space.Name.Split(" - ")[1]}, {app.Config.Name}");
+                await _mysql.CreatePodioAppView(input.appId, input.version, space.Name.Split(" - ")[1], app.Config.Name)
+                            .ContinueWith(async t => await _mysql.CreatePodioAppTable(input.appId, input.version, space.Name.Split(" - ")[1], app.Config.Name))
+                            .ContinueWith(async t => await _mysql.RebuildAppTable(input.appId, input.version, 'Y'));
+            }
         }
     }
 }
