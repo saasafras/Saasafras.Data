890bf6cf33d79c81d6c872411d68803dc5ea551a
alex <alex@brickbridgeconsulting.com>
2018-10-30 13:33:49 -0400

    nuget, base, role

--- DeleteItemHandler/src/DeleteItemHandler/DeleteItemFunction.cs
+++ DeleteItemHandler/src/DeleteItemHandler/DeleteItemFunction.cs
@@ -2,26 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using BrickBridge.Models;
 using Amazon.Lambda.Core;
+using BrickBridge.Lambda.MySql;
+using PodioCore.Models;
+using PodioCore.Utils.ItemFields;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
-
-namespace DeleteItemHandler
+namespace BrickBridge.Lambda
 {
-    public class Function
+    public class DeleteItemFunction
     {
-        
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public async System.Threading.Tasks.Task FunctionHandler(RoutedPodioEvent input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            context.Logger.LogLine($"Entered function...");
+            context.Logger.LogLine($"AppId: {input.appId}");
+            context.Logger.LogLine($"ClientId: {input.clientId}");
+
+            //var spaceId = input.currentItem.App.SpaceId.Value;
+            //context.Logger.LogLine($"SpaceId: {spaceId}");
+
+            //var deployment = input.currentEnvironment.apps.First(a => a.appId == input.appId);
+            //context.Logger.LogLine($"Deployment: {deployment.date}");
+
+            //var spaceName = deployment.deployedSpaces.First(kv => kv.Value == spaceId.ToString()).Key;
+            //context.Logger.LogLine($"Space Name: {spaceName}");
+
+            //var appName = input.currentItem.App.Name;
+
+            using (var _mysql = new MySqlQueryHandler(context))
+            {
+                context.Logger.LogLine($"Deleting item {input.podioEvent.item_id}.");
+                try
+                {
+                    await _mysql.DeletePodioItem(int.Parse(input.podioEvent.item_id));
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine($"ItemId: {input.podioEvent.item_id}");
+                    throw e;
+                }
+            }
         }
     }
 }
